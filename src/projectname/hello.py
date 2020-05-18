5f88a1f555f101ad8ae2d508cf789094a808e2ff
Scott Davis <stdavis@utah.gov>
2020-05-18 17:15:43 -0600

    chore: copy most settings from agrc/open-sgid

---
+++ src/projectname/hello.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# * coding: utf8 *
+'''
+a description of what this module does
+'''
+
+import arcpy
+#: this file is for testing linting...
+
+
+TEST = "test"
+
+def hello():
+    print('this is good')
