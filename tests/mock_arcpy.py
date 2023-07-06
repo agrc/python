c11b33cf21e9501c73d65ab86b18a0cf23ba0820
steveoh <sgourley@utah.gov>
2023-07-06 14:59:25 -0600

    style: black formatting

--- tests/mock_arcpy.py
+++ tests/mock_arcpy.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# * coding: utf8 *
 """This allows you to mock out the entire arcpy package so that you can test packages that use arcpy in github's CI
 environment. You just need to 'import mock_arcpy' prior to importing arcpy directly or importing any other packages/
 modules that import arcpy in turn.
@@ -6,6 +8,6 @@ modules that import arcpy in turn.
 import sys
 from unittest.mock import Mock
 
-MODULE_NAME = 'arcpy'
+MODULE_NAME = "arcpy"
 arcpy = Mock(name=MODULE_NAME)
 sys.modules[MODULE_NAME] = arcpy
