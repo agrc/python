79146d004f3b9c8187b583733da7210854449272
stdavis <stdavis@utah.gov>
2025-01-23 16:20:29 -0700

    ci: standardize actions

--- src/projectname/version.py
+++ src/projectname/version.py
@@ -1,6 +1,3 @@
-"""A single source of truth for the version in a programmatically-accessible variable.
-
-This must only include a single line: __version__ = 'x.y.z'
-"""
+"""A single source of truth for the version in a programmatically-accessible variable."""
 
 __version__ = "1.0.2"
